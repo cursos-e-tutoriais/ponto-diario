c344803dc612218b02841f0df3cacc32a4000167
Jefferson Rodrigues <rodriguesjeff.dev@gmail.com>
2021-01-11 20:31:37 -0300

    Primeiro commit

---
+++ android/app/src/main/kotlin/com/startcomdev/ponto_diario/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.startcomdev.ponto_diario
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,17 @@
+import 'package:ponto_diario/app/modules/home/views/home_view.dart';
+import 'package:ponto_diario/app/modules/home/bindings/home_binding.dart';
+import 'package:get/get.dart';
+part 'app_routes.dart';
+
+class AppPages {
+  
+static const INITIAL = Routes.HOME;
+
+  static final routes = [
+    GetPage(
+      name: Routes.HOME, 
+      page:()=> HomeView(), 
+      binding: HomeBinding(),
+    ),
+  ];
+}
