60bc6e1d5d243ce30f4622bcab5750ec320d1e77
Jefferson Rodrigues <rodriguesjeff.dev@gmail.com>
2021-01-11 22:51:15 -0300

    agora pegamos a localização atual do usuário, próximo passo será atualizar o mapa para a localização atual.

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,12 +1,42 @@
+import 'package:flutter_map/plugin_api.dart';
 import 'package:get/get.dart';
+import 'package:location/location.dart';
 
 class HomeController extends GetxController {
   
   final String urlMap = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
   final subdomains = ['a', 'b', 'c'];
+  MapController mapController = MapController();
+  Location location = Location();
+  bool _serviceEnabled;
+  PermissionStatus _permissionGranted;
+  LocationData _locationData;
 
   @override
-  void onInit() {}
+  void onInit() async {
+    super.onInit();
+    await checkPermission();
+    _locationData = await location.getLocation();
+    print(_locationData);
+    
+  }
+
+  checkPermission() async {
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+    } else {
+      return;
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied){
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted == PermissionStatus.granted) {
+        return;
+      }
+    }
+  }
 
   @override
   void onReady() {}
