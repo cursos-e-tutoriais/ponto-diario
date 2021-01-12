6e90e4c9f168e6b14c9ba91112454ca0b70f9a09
Jefferson Rodrigues <rodriguesjeff.dev@gmail.com>
2021-01-11 21:07:08 -0300

    criado e adicionado mapa do openstreetmap na tela inicial

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart'; 
+import 'package:flutter_map/flutter_map.dart';
+import 'package:get/get.dart';
+import "package:latlong/latlong.dart" as latLng;
 import 'package:ponto_diario/app/modules/home/controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -10,13 +12,30 @@ class HomeView extends GetView<HomeController> {
         title: Text('HomeView'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'HomeView is working', 
-          style: TextStyle(fontSize:20),
+      body: FlutterMap(
+        options: MapOptions(
+          center: latLng.LatLng(51.5, -0.09),
+          zoom: 13.0
         ),
+        layers: [
+          TileLayerOptions(
+            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            subdomains: ['a', 'b', 'c'],
+          ),
+          MarkerLayerOptions(
+            markers: [
+              Marker(
+                width: 80.0,
+                height: 80.0,
+                point: latLng.LatLng(51.5, -0.09),
+                builder: (_) => Container(
+                  child: FlutterLogo(),
+                )
+              )
+            ]
+          )
+        ],
       ),
     );
   }
 }
-  
